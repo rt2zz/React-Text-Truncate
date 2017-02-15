40a428049f8eeff98be417dfa467c13278b96db5
rt2zz <zack@root-two.com>
2017-02-14 18:58:59 -0800

    wip, webpack clamp + api refactor

--- src/TextTruncate.js
+++ src/TextTruncate.js
@@ -1,166 +1,143 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
-export default class TextTruncate extends Component {
-    static propTypes = {
-        containerClassName: React.PropTypes.string,
-        line: React.PropTypes.number,
-        text: React.PropTypes.string,
-        textTruncateChild: React.PropTypes.node,
-        truncateText: React.PropTypes.string
-    };
+const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)
+const isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor)
+const isWebkit = isChrome || isSafari
 
-    static defaultProps = {
-        line: 1,
-        text: '',
-        truncateText: '…'
-    };
+const truncateText = '…'
+const containerStyle = {width: '100%', height: '100%', overflow: 'hidden'}
 
-    componentDidMount() {
-        const canvas = document.createElement('canvas');
-        const docFragment = document.createDocumentFragment();
-        const style = window.getComputedStyle(this.refs.scope);
-        const font = [
-            style['font-weight'],
-            style['font-style'],
-            style['font-size'],
-            style['font-family']
-        ].join(' ');
+export default class LineClamp extends Component {
+  static propTypes = {
+    lines: React.PropTypes.number,
+    text: React.PropTypes.string,
+  }
 
-        docFragment.appendChild(canvas);
-        this.canvas = canvas.getContext('2d');
-        this.canvas.font = font;
-        this.forceUpdate();
-        window.addEventListener('resize', this.onResize);
+  static defaultProps = {
+    lines: 1,
+    text: '',
+  }
+
+  state = {
+    trimmedText: null,
+  }
+
+  update = () => {
+    this.forceUpdate()
+  }
+
+  processRef = (scope) => {
+    if (scope && !isWebkit) this.setRenderText(scope)
+  }
+
+  measureWidth(text) {
+    return this.canvas.measureText(text).width
+  }
+
+  setRenderText(scope) {
+    const {
+      lines,
+      text,
+    } = this.props
+
+    const canvas = document.createElement('canvas')
+    const docFragment = document.createDocumentFragment()
+    const style = window.getComputedStyle(scope)
+    const font = [
+      style['font-weight'],
+      style['font-style'],
+      style['font-size'],
+      style['font-family']
+    ].join(' ')
+
+    docFragment.appendChild(canvas)
+    this.canvas = canvas.getContext('2d')
+    this.canvas.font = font
+
+    const scopeWidth = scope.getBoundingClientRect().width
+
+    // return if display:none
+    if (scopeWidth === 0) {
+      return null
     }
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.onResize);
+    // return if all of text can be displayed
+    if (scopeWidth >= this.measureWidth(text)) {
+      this.setState({ trimmedText: text })
+      return
     }
 
-    onResize = () => {
-        window.requestAnimationFrame(this.update.bind(this))
-    };
+    let childText = ''
+    let currentPos = 1
+    let maxTextLength = text.length
+    let truncatedText = ''
+    let splitPos = 0
+    let startPos = 0
+    let displayLine = lines
+    let width = 0
+    let lastIsEng = false
+    let lastSpaceIndex = -1
 
-    update = () => {
-        this.forceUpdate();
-    };
-
-    measureWidth(text) {
-        return this.canvas.measureText(text).width;
-    }
-
-    getRenderText() {
-        const {
-            containerClassName,
-            line,
-            text,
-            textTruncateChild,
-            truncateText,
-            ...props
-        } = this.props;
-
-        const scopeWidth = this.refs.scope.getBoundingClientRect().width;
-
-        // return if display:none
-        if (scopeWidth === 0) {
-            return null;
-        }
-
-        // return if all of text can be displayed
-        if (scopeWidth >= this.measureWidth(text)) {
-            return (
-                <div {...props}>{text}</div>
-            );
-        }
-
-        let childText = '';
-        if (textTruncateChild && typeof textTruncateChild.type === 'string') {
-            let type = textTruncateChild.type;
-            if (type.indexOf('span') >= 0 || type.indexOf('a') >= 0) {
-                childText = textTruncateChild.props.children;
+    while (displayLine--) {
+      let ext = displayLine ? '' : truncateText + ' ' + childText
+      while (currentPos <= maxTextLength) {
+        truncatedText = text.substr(startPos, currentPos)
+        width = this.measureWidth(truncatedText + ext)
+        if (width < scopeWidth) {
+          splitPos = text.indexOf(' ', currentPos + 1)
+          if (splitPos === -1) {
+            currentPos += 1
+            lastIsEng = false
+          } else {
+            lastIsEng = true
+            currentPos = splitPos
+          }
+        } else {
+          do  {
+            currentPos--
+            truncatedText = text.substr(startPos, currentPos)
+            if (truncatedText[truncatedText.length - 1] === ' ') {
+              truncatedText = text.substr(startPos, currentPos - 1)
             }
-        }
-
-        let currentPos = 1;
-        let maxTextLength = text.length;
-        let truncatedText = '';
-        let splitPos = 0;
-        let startPos = 0;
-        let displayLine = line;
-        let width = 0;
-        let lastIsEng = false;
-        let lastSpaceIndex = -1;
-
-        while (displayLine--) {
-            let ext = displayLine ? '' : truncateText + ' ' + childText;
-            while (currentPos <= maxTextLength) {
-                truncatedText = text.substr(startPos, currentPos);
-                width = this.measureWidth(truncatedText + ext);
-                if (width < scopeWidth) {
-                    splitPos = text.indexOf(' ', currentPos + 1);
-                    if (splitPos === -1) {
-                        currentPos += 1;
-                        lastIsEng = false;
-                    } else {
-                        lastIsEng = true;
-                        currentPos = splitPos;
-                    }
-                } else {
-                    do  {
-                        currentPos--;
-                        truncatedText = text.substr(startPos, currentPos);
-                        if (truncatedText[truncatedText.length - 1] === ' ') {
-                            truncatedText = text.substr(startPos, currentPos - 1);
-                        }
-                        if (lastIsEng) {
-                            lastSpaceIndex = truncatedText.lastIndexOf(' ');
-                            if (lastSpaceIndex > -1) {
-                                currentPos = lastSpaceIndex;
-                                truncatedText = text.substr(startPos, currentPos);
-                            }
-                        }
-                        width = this.measureWidth(truncatedText + ext);
-                    } while (width >= scopeWidth);
-                    startPos += currentPos;
-                    break;
-                }
+            if (lastIsEng) {
+              lastSpaceIndex = truncatedText.lastIndexOf(' ')
+              if (lastSpaceIndex > -1) {
+                currentPos = lastSpaceIndex
+                truncatedText = text.substr(startPos, currentPos)
+              }
             }
-
-            if (currentPos >= maxTextLength) {
-                startPos = maxTextLength;
-                break;
-            }
+            width = this.measureWidth(truncatedText + ext)
+          } while (width >= scopeWidth)
+          startPos += currentPos
+          break
         }
+      }
 
-        if (startPos === maxTextLength) {
-            return (
-                text
-            );
-        }
-        return (
-            <div {...props}>
-                {text.substr(0, startPos) + truncateText + ' '}
-                {textTruncateChild}
-            </div>
-        );
-
+      if (currentPos >= maxTextLength) {
+        startPos = maxTextLength
+        break
+      }
     }
 
-    render() {
-        const {
-            text,
-            containerClassName,
-        } = this.props;
-
-        let renderText = text;
-        if (this.refs.scope) {
-            renderText = this.getRenderText();
-        }
-
-        return (
-            <div ref='scope' className={containerClassName} style={{overflow: 'hidden'}}>
-                {renderText}
-            </div>
-        );
+    if (startPos === maxTextLength) {
+      this.setState({ trimmedText: text })
+      return
+    } else {
+      this.setState({ trimmedText: truncatedText + truncateText })
+      return
     }
-};
+  }
+
+  render() {
+    const { lines, text, ...domProps } = this.props
+    let style = !isWebkit
+      ? containerStyle
+      : { ...containerStyle, display: '-webkit-box', WebkitLineClamp: lines, 'something': 1, WebkitBoxOrient: 'vertical' }
+
+    return (
+      <div {...domProps} ref={this.processRef} style={style}>
+        {this.state.trimmedText || text}
+      </div>
+    )
+  }
+}
